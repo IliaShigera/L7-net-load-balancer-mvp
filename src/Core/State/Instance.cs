2573626d567a32a40c24000f70b3f74bcb2c82de
Ilia <ShigeraIlia@gmail.com>
2025-05-04 15:01:14 -0400

    fix thread safety issue for instance health flag

--- src/Core/State/Instance.cs
+++ src/Core/State/Instance.cs
@@ -4,11 +4,10 @@ public sealed class Instance
 {
     public required string Name { get; init; }
     public required string Address { get; init; }
-    
-    // todo: is this safe ?
-    private bool _isHealthy;
-    public bool IsHealthy => _isHealthy;
 
-    public void MarkHealthy() => _isHealthy = true;
-    public void MarkUnhealthy() => _isHealthy = false;
+    private int _isHealthy = -1;
+    public bool IsHealthy => Volatile.Read(ref _isHealthy) == 1;
+
+    public void MarkHealthy() => Volatile.Write(ref _isHealthy, 1);
+    public void MarkUnhealthy() => Volatile.Write(ref _isHealthy, 0);
 }
